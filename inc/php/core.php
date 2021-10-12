1cd44ebebb1e6dfa1e714b35e51bd890ddb20e4b
Arthur Gareginyan <arthurgareginyan@gmail.com>
2021-10-13 00:15:04 +0300

    Maintenance: Code formatting improved

--- inc/php/core.php
+++ inc/php/core.php
@@ -134,7 +134,7 @@ function spacexchimp_p020_activation() {
     }
 
     // Get the activation date of the plugin from the database
-    $activation_date = !empty( $info['activation_date'] ) ? $info['activation_date'] : '';
+    $activation_date = ! empty( $info['activation_date'] ) ? $info['activation_date'] : '';
 
     if ( $activation_date == '' ) {
         $info['activation_date'] = time();
